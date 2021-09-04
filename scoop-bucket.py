1c117371fdba302a71034513b5c1ecc2e00fe1e5
zp <zp.c@outlook.com>
2021-09-04 21:40:15 +0800

    update scoop

--- scoop-bucket.py
+++ scoop-bucket.py
@@ -39,17 +39,19 @@ class Package(Soft):
             for file in os.listdir(bucket):
                 if file.endswith('.json'):
                     data = load(str(bucket/file))
+                    data.mpkg_src = f'https://github.com/{repo}/blob/master/bucket/{file}'
                     if data:
                         self.packages.append(data.asdict(simplify=True))
 
         names = [data['id'] for data in self.packages]
+        names_l = [data['id'].lower() for data in self.packages]
         conflicted = [(i, n)
-                      for i, n in enumerate(names) if names.count(n) > 1]
+                      for i, n in enumerate(names) if names_l.count(n.lower()) > 1]
         if conflicted:
             print(f'scoop id conflict: {conflicted}')
-        dict_ = dict([(n, []) for i, n in conflicted])
+        dict_ = dict([(n.lower(), []) for i, n in conflicted])
         for i, n in conflicted:
-            dict_[n].append(i)
+            dict_[n.lower()].append(i)
         for k, v in dict_.items():
             for i in v:
                 self.packages[i]['id'] = f'{k}_{v.index(i)}'
